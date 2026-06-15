6a564119d06c5609ce161df8c6205e4bdcef853b
xd <202213866g@utea.edu.pe>
2026-06-15 11:28:53 -0500

    cleancode para el archivo functions.ts

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -1,27 +1,36 @@
 (() => {
 
     // función para obtener información de una película por Id
-    function getAllMovies( movieId: string ) {
-        console.log({ movieId });
+    function getMovieById( id: string ) {
+        console.log({ id });
     }
 
-    // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
-    function getAllMovieActors( id: string ) {
+    // función para obtener iformación de los actores de una película - Actors o Cast // id = movieId getMovieCast
+    function getMovieCastById( id: string ) {
         console.log({ id });
     }
 
     // funcion para obtener el bio del actor por el id
-    function getUsuario( ActorId: string ) {
-        console.log({ ActorId });
+    function getActorBioById( id: string ) {
+        console.log({ id });
     }
     
     // Crear una película
-    function movie(title: string, description: string, rating: number, cast: string[] ) {
+    //una buena funcion tiene que tener 3 parametros por eso se crea un interface
+
+    interface Movie {
+        title: string;
+        description: string;
+        rating: number;
+        cast: string[];
+    }
+
+    function CreateMovie({title, description, rating, cast}: Movie) {
         console.log({ title, description, rating, cast });
     }
 
     // Crea un nuevo actor
-    function createActorIfActorNotExists( fullName: string, birthdate: Date ): boolean {
+    function createActor( fullName: string, birthdate: Date ): boolean {
         
         // tarea asincrona para verificar nombre
         // ..
@@ -32,8 +41,5 @@
         return true;        
 
     }
-
-    
-
 
 })();
