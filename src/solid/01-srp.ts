3c565ca31a4f37dab990efb290117dc173d436fa
xd <202213866g@utea.edu.pe>
2026-06-16 09:19:33 -0500

    principio 1 solid

--- src/solid/01-srp.ts
+++ src/solid/01-srp.ts
@@ -18,31 +18,32 @@
             // Realiza una petición para salvar en base de datos 
             console.log('Guardando en base de datos', product );
         }
-    
         notifyClients() {
             console.log('Enviando correo a los clientes');
         }
     
-        onAddToCart( productId: number ) {
+        /*onAddToCart( productId: number ) {
+            // Agregar al carrito de compras
+            console.log('Agregando al carrito ', productId );
+        }*/
+    
+    }
+
+    class CartBloc{
+        addToCart( productId: number ) {
             // Agregar al carrito de compras
             console.log('Agregando al carrito ', productId );
         }
-    
     }
-    
 
 
     const productBloc = new ProductBloc();
+    const cartBloc = new CartBloc();
 
     productBloc.loadProduct(10);
     productBloc.saveProduct({ id: 10, name: 'OLED TV' });
     productBloc.notifyClients();
-    productBloc.onAddToCart(10);
-
-
-
-
-
+    cartBloc.addToCart(10);
 
 
 
